56c18a9ef6b8ffcc3d60ec450f94e01d6560b7d2
Shyam <shyam85509@gmail.com>
2024-02-08 13:10:26 +0530

    Add project files.

---
+++ Controllers/ConsumeApiCrudController.cs
@@ -0,0 +1,123 @@
+﻿using ConsumeWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsumeWebApi.Controllers
+{
+    public class ConsumeApiCrudController : Controller
+    {
+        public async Task<IEnumerable<StudentsData>>  GetAllData()
+        {
+            IEnumerable<StudentsData> obj = null;
+            using(HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7011/api/");
+                var responce = client.GetAsync("Crudstd/GetAllData");
+                responce.Wait();
+
+                var data = responce.Result;
+                if (data.IsSuccessStatusCode)
+                {
+                    var read = data.Content.ReadAsAsync<List<StudentsData>>();
+                    read.Wait();
+                    obj = read.Result;
+                }
+            }
+            return obj;
+        }
+
+        public async Task<IActionResult> ShowData()
+        {
+            IEnumerable<StudentsData> obj = await GetAllData();
+            return View(obj);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(StudentsData obj)
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7011/api/");
+                var postData = client.PostAsJsonAsync<StudentsData>("Crudstd/InsetStd", obj);
+                var res = postData.Result;
+                if (res.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("ShowData", "ConsumeApiCrud");
+                }
+                else
+                {
+                    ModelState.AddModelError("","Something wrong");
+                }
+            }
+            return View();
+        }
+
+        public IActionResult Detials()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            StudentsData obj = null;
+            using(HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7011/api/");
+                var responce = client.GetAsync("Crudstd/GetDataById?Id="+Id);
+                var res = responce.Result;
+                if (res.IsSuccessStatusCode)
+                {
+                    var data = res.Content.ReadAsAsync<StudentsData>();
+                    obj = data.Result;
+                }
+            }
+            return View(obj);
+        }
+
+        
+
+        [HttpPost]
+        public IActionResult Delete(int Id, StudentsData obj)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7011/api/");
+
+            }
+                return View();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Models/StudentsData.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsumeWebApi.Models
+{
+    public class StudentsData
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int recordId { get; set; }
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        public string EmailID { get; set; }
+        public string Password { get; set; }
+        public byte Age { get; set; }
+        public DateTime Dob { get; set; }
+        public decimal fee { get; set; }
+        public string Dept { get; set; }
+        public Boolean status { get; set; }
+        public string role { get; set; }
+    }
+}
